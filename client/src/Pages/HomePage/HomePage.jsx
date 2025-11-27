5ed0a9226e5ce607eb25093055102188338d3247
AliMustafa010 <alimustafakhan121@gmail.com>
2025-11-27 19:05:48 +0500

    Start

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import './HomePage.css'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import ChatContainer from '../../Components/ChatContainer/ChatContainer'
-import RightSidebar from '../../Components/RightSidebar/RightSidebar'
 
 const HomePage = () => {
   return (
-    <div className='home-page'>
-        <div className='sidebar'>
-            <Sidebar />
-        </div>
-        <div className="chat-container">
-            <ChatContainer />
-        </div>
-        <div className="right-sidebar">
-            <RightSidebar />
-        </div>
+    <div>
+        This is Home
     </div>
   )
 }
