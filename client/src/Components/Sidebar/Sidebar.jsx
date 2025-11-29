6e517201f98173b3a7f98a7b0636c68feb5c7f91
AliMustafa010 <alimustafakhan121@gmail.com>
2025-11-29 17:10:35 +0500

    Add Item Selected Option

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -1,58 +1,80 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import  avatar from "../../Asset/avatar.jpg"
-import "./Sidebar.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import avatar from "../../Asset/avatar.jpg";
+import "./Sidebar.css";
 
 import { IoHomeOutline, IoLogOutOutline } from "react-icons/io5";
-import { FaRegMessage } from "react-icons/fa6";
-import { GoBell, GoGear } from "react-icons/go";
 import { LuMessageSquare } from "react-icons/lu";
-
+import { GoBell, GoGear } from "react-icons/go";
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
+    const [selected, setSelected] = useState(null);
 
-        {/* Sidebar Image */}
-        <div className="sidebar-profile">
-            <Link to="">
-                <img src={avatar} alt="profile" className='sidebar-profile-img' />
-            </Link>      
+    const handleSelect = (item) => {
+        setSelected(item);
+    };
+
+    return (
+        <div className="sidebar">
+
+            {/* Sidebar Image */}
+            <div className="sidebar-profile">
+                <Link to="">
+                    <img src={avatar} alt="profile" className="sidebar-profile-img" />
+                </Link>
+            </div>
+
+            {/* Sidebar Items */}
+            <div className="sidebar-items">
+                <div
+                    className={"sidebar-item-1" + (selected === "item1" ? " selected-sidebar-item" : "")}
+                    onClick={() => handleSelect("item1")}
+                >
+                    <span className="sidebar-item-span"></span>
+                    <Link to="">
+                        <IoHomeOutline style={{ fontSize: "30px", color: "white" }} />
+                    </Link>
+                </div>
+
+                <div
+                    className={"sidebar-item-2" + (selected === "item2" ? " selected-sidebar-item" : "")}
+                    onClick={() => handleSelect("item2")}
+                >
+                    <span className="sidebar-item-span"></span>
+                    <Link to="">
+                        <LuMessageSquare style={{ fontSize: "30px", color: "white" }} />
+                    </Link>
+                </div>
+
+                <div
+                    className={"sidebar-item-3" + (selected === "item3" ? " selected-sidebar-item" : "")}
+                    onClick={() => handleSelect("item3")}
+                >
+                    <span className="sidebar-item-span"></span>
+                    <Link to="">
+                        <GoBell style={{ fontSize: "30px", color: "white" }} />
+                    </Link>
+                </div>
+
+                <div
+                    className={"sidebar-item-4" + (selected === "item4" ? " selected-sidebar-item" : "")}
+                    onClick={() => handleSelect("item4")}
+                >
+                    <span className="sidebar-item-span"></span>
+                    <Link to="">
+                        <GoGear style={{ fontSize: "30px", color: "white" }} />
+                    </Link>
+                </div>
+            </div>
+
+            {/* Sidebar Logout */}
+            <div className="sidebar-logout">
+                <Link to="">
+                    <IoLogOutOutline style={{ fontSize: "30px", color: "white" }} />
+                </Link>
+            </div>
         </div>
+    );
+};
 
-        {/* Sidebar Items */}
-        <div className="sidebar-items">
-            <div className="sidebar-item-1">
-                <Link to="">
-                    <IoHomeOutline style={{fontSize : "30px" , color : "white"}} />
-                </Link>
-            </div>
-            <div className="sidebar-item-2">
-                <Link to="">
-                    <LuMessageSquare style={{fontSize : "30px" , color : "white" }} />
-                </Link>
-            </div>
-            <div className="sidebar-item-3">
-                <Link to="">
-                    <GoBell style={{fontSize : "30px" , color : "white"}} />
-                </Link>
-            </div>
-            <div className="sidebar-item-4">
-                <Link to="">
-                    <GoGear style={{fontSize : "30px" , color : "white"}} />
-                </Link>
-            </div>
-        </div>
-
-        {/* Sidebar Logout */}
-        <div className="sidebar-logout">
-            <Link to="">
-                <IoLogOutOutline style={{fontSize : "30px" , color : "white"}} />
-            </Link>
-        </div>
-
-    </div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
