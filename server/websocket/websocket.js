18d9150ae435da565581f5290046254b1edaf266
AliMustafa010 <alimustafakhan121@gmail.com>
2025-11-30 21:30:58 +0500

    reverse

--- server/websocket/websocket.js
+++ server/websocket/websocket.js
@@ -8,12 +8,7 @@ function setupWebSocket(server) {
 
     ws.on('message', (message) => {
       console.log('Received: %s', message);
-
-      wss.clients.forEach(client => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
-        }
-      });
+      ws.send(`Server: Got your message "${message}"`);
     });
 
     ws.on('close', () => {
